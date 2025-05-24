c89439fd1b5c862a57f15abafe831b2d94fab5cf
Daniel Košč <daniel.kosc@gmail.com>
2025-05-24 16:27:15 +0200

    Refact: better error handling

--- classes.py
+++ classes.py
@@ -2,24 +2,28 @@ from summary import get_summary
 
 
 class Paper:
-    def __init__(self, json_element) -> None:
-
-        self.title = json_element['title']['title']
-        abstract = ""
-        try:
-            abstract = json_element['abstract']['summary']
-        except Exception as e:
-            print('No abstract for ' + self.title)
-        self.abstract = abstract
+    def __init__(self, json_element: dict) -> None:
+        self.title = json_element.get('title', {}).get('title', 'Untitled Paper')
+        self.abstract = json_element.get('abstract', {}).get('summary', '')
+        if not self.abstract:
+            print(f"Warning: No abstract found for '{self.title}'.")
         self.url = None
-        self.summary = get_summary(self.abstract)
-        files = json_element['files']
-        for file in files:
-            if file['full_name'].endswith('.pdf'):
-                self.url = file['url']
+        pdf_files = [file for file in json_element.get('files', []) if
+                     file.get('eformat') and '.pdf' in file.get('eformat').lower()]
+        if pdf_files:
+            self.url = pdf_files[0].get('url')
+        if len(pdf_files) > 1:
+            print(f"Warning: {len(pdf_files)} PDF files found for '{self.title}'. Using first other are ignored!")
+
+        self.summary = get_summary(self.abstract) if self.abstract else "No summary available."
 
     def __repr__(self) -> str:
-        return f"{'title':'{self.title}',  'url':'{self.url}', 'summary':{self.summary}}"
+        return (
+            f"{{'title': '{self.title}', "
+            f"'url': '{self.url}', "
+            f"'summary': '{self.summary}'}}"
+        )
+
 
 class Video:
     def __init__(self, json_element) -> None:
